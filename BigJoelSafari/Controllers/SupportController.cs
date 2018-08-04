d51b18f04184a6bd308805435f835db68ee90719
Jessica Nations <jessica.nations@gmail.com>
2018-08-04 13:07:13 -0500

    the links work!

---
+++ BigJoelSafari/Controllers/SupportController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace BigJoelSafari.Controllers
+{
+    public class SupportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BigJoelSafari/Controllers/VisitController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace BigJoelSafari.Controllers
+{
+    public class VisitController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
