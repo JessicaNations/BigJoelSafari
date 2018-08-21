3de01dad84fd8c25a48e4a2c9471c5a1e8725043
Jessica Nations <jessica.nations@gmail.com>
2018-08-21 09:41:48 -0500

    save

---
+++ BigJoelSafari/Controllers/PartiesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace BigJoelSafari.Controllers
+{
+    public class PartiesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
